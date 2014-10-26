882e16716e0c5b3732a217131fa3fc65897f0648
Christian <Christian-krogh@hotmail.com>
2014-10-26 17:56:57 +0100

    * Yoshis Island 1 (Level 1) almost completed

---
+++ Assets/Game_SuperMario2D/Scripts/script_oneWayCollision.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+	//the collider of the main visible platform
+	//var collider : BoxCollider;
+	//this variable is true when the players is just below the platform so that its Box collider can be disabled that will allow the player to pass through the platform
+	var oneway : boolean;
+
+	var parentCollider : BoxCollider;
+
+function Start () 
+{
+	parentCollider = this.transform.parent.transform.GetComponent(BoxCollider);
+}
+
+function Update () 
+{
+	//Enabling or Disabling the platform's Box collider to allowing player to pass
+	if (oneway)
+	  parentCollider.enabled=false;
+	 if (!oneway)
+	  parentCollider.enabled=true;   
+}
+	//Checking the collison of the gameobject we created in step 2 for checking if the player is just below the platform and nedded to ignore the collison to the platform
+function OnTriggerStay(other: Collider) 
+{
+     oneway = true;
+}
+
+function OnTriggerExit(other: Collider) 
+{
+	//Just to make sure that the platform's Box Collider does not get permantly disabled and it should be enabeled once the player get its through
+	oneway = false;
+}
